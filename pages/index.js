ef5750b66484974b42a69824d8ea12589af30859
Daniel Ricardo <100872200+danricardo88@users.noreply.github.com>
2024-01-25 19:41:01 +0000

    add dir pages

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+const Home = () => {
+
+    return <h1>Filha, seu papaizinho por você vai tentar, fazer, estudar e atravessar todas as dificuldades do mundo por você !</h1>
+};
+
+export default Home;
