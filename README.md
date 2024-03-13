93d767de805958f3fff91722f62de39074348d7f
danricardo88 <daniel.ricardo88@outlook.com>
2024-03-13 11:45:16 -0300

    adicona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do https://tabnews.com.br para fins de estudo e aprimoramento do meu conhecimento. 
+
+Implementação do https://tabnews.com.br para fins de estudo e aprimoramento do meu conhecimento.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 const Home = () => {
-
-    return <h1>Dedicando todas as minhas forças para proporcionar uma vida melhor para minha filha!</h1>
+  return (
+    <h1>
+      Dedicando todas as minhas forças para proporcionar uma vida melhor para
+      minha filha!
+    </h1>
+  );
 };
 
 export default Home;
